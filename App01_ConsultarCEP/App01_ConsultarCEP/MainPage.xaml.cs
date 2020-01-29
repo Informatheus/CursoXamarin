da2e249a9476c0135643e41c58dd5c53c3319639
Matheus Henrique da Silva <matheustr@hotmail.com>
2020-01-29 20:14:08 -0300

    bla

---
+++ App01_ConsultarCEP/App01_ConsultarCEP/MainPage.xaml.cs
@@ -0,0 +1,39 @@
+﻿using App01_ConsultarCEP.Servico;
+using App01_ConsultarCEP.Servico.Modelo;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace App01_ConsultarCEP
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            Botao.Clicked += BuscarCEP;
+
+        }
+
+        private void BuscarCEP(object sender, EventArgs args)
+        {
+            //TODO Lógica do programa
+
+            // Fazer validações
+
+            string cep = CEP.Text.Trim();
+            Endereco end = ViaCEPServico.BuscarEnderecoViaCEP(cep);
+
+            Resultado.Text = string.Format("Endereço: {0}, {1} {2}", end.localidade, end.uf, end.logradouro);
+
+        }
+    }
+}
