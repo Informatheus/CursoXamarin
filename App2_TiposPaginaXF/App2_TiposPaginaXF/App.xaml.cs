ff9a12939f14fb808c12c946b544cf3616538112
Matheus Henrique da Silva <matheustr@hotmail.com>
2020-01-30 12:59:12 -0300

    first push

---
+++ App2_TiposPaginaXF/App2_TiposPaginaXF/App.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace App2_TiposPaginaXF
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            //MainPage = new MainPage();
+            MainPage = new App2_TiposPaginaXF.Paginas.Carousel.Page3();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
