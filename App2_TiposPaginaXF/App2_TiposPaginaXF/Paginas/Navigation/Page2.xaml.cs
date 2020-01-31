a24aa79ce48d4b557c97a80a2205671790a0898e
Matheus Henrique da Silva <matheustr@hotmail.com>
2020-01-30 23:21:02 -0300

    Abas na mesma NavigationPage do resto do app

--- App2_TiposPaginaXF/App2_TiposPaginaXF/Paginas/Navigation/Page2.xaml.cs
+++ App2_TiposPaginaXF/App2_TiposPaginaXF/Paginas/Navigation/Page2.xaml.cs
@@ -1,15 +1,21 @@
 ﻿
+using System;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
-namespace App2_TiposPaginaXF.Paginas.Navigation
-{
+namespace App2_TiposPaginaXF.Paginas.Navigation {
     [XamlCompilation(XamlCompilationOptions.Compile)]
-    public partial class Page2 : ContentPage
-    {
-        public Page2()
-        {
+    public partial class Page2 : ContentPage {
+        public Page2() {
             InitializeComponent();
+        }
+
+        private void OnButtonClicked(object sender, EventArgs args) {
+            //abrir com retorno
+            //App.Current.MainPage = new NavigationPage(new Tabbed.Abas());
+
+            //abrir sem retorno
+            Navigation.PushAsync(new Tabbed.Abas());
         }
     }
 }
---
+++ App2_TiposPaginaXF/App2_TiposPaginaXF/Paginas/Tabbed/Abas.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace App2_TiposPaginaXF.Paginas.Tabbed {
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Abas : TabbedPage {
+        public Abas() {
+            InitializeComponent();
+
+            //adiciona abas dinamicamente
+            //Children.Add(new NavigationPage(new Page2()) { Title = "Aba 2" });
+        }
+    }
+}
