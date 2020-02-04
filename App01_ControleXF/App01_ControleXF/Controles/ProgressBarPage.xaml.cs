b5ea0cc6468015396517de992ce33bfeab2f3593
Matheus Henrique da Silva <matheustr@hotmail.com>
2020-02-04 18:13:25 -0300

    App de controles & views até a parte button

---
+++ App01_ControleXF/App01_ControleXF/Controles/ProgressBarPage.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace App01_ControleXF.Controles {
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ProgressBarPage : ContentPage {
+        public ProgressBarPage() {
+            InitializeComponent();
+
+            Label1.Text="BounceIn";
+            Label2.Text = "BounceOut";
+            Label3.Text = "CubicIn";
+            Label4.Text = "CubicInOut";
+            Label5.Text = "CubicOut";
+            Label6.Text = "SinIn";
+            Label7.Text = "SinInOut";
+            Label8.Text = "SinOut";
+            Label9.Text = "SpringIn";
+            Label10.Text = "SpringOut";
+        }
+
+        private void Modificar(object sender, EventArgs args) {
+            Bar1.ProgressTo(1, 15000, Easing.BounceIn);
+            Bar2.ProgressTo(1, 15000, Easing.BounceOut);
+            Bar3.ProgressTo(1, 15000, Easing.CubicIn);
+            Bar4.ProgressTo(1, 15000, Easing.CubicInOut);
+            Bar5.ProgressTo(1, 15000, Easing.CubicOut);
+            Bar6.ProgressTo(1, 15000, Easing.SinIn);
+            Bar7.ProgressTo(1, 15000, Easing.SinInOut);
+            Bar8.ProgressTo(1, 15000, Easing.SinOut);
+            Bar9.ProgressTo(1, 15000, Easing.SpringIn);
+            Bar10.ProgressTo(1, 15000, Easing.SpringOut);
+
+        }
+    }
+}
---
+++ App01_ControleXF/App01_ControleXF/Menu/Master.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace App01_ControleXF.Menu {
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Master : MasterDetailPage {
+        public Master() {
+            InitializeComponent();
+        }
+
+        private void GoToActivityIndicator(object sender, EventArgs args) {
+            Detail = new Controles.ActivityIndicatorPage();
+            IsPresented = false;
+        }
+
+        private void GoToProgressBar(object sender, EventArgs args) {
+            Detail = new Controles.ProgressBarPage();
+            IsPresented = false;
+        }
+
+        private void GoToBoxView(object sender, EventArgs args) {
+            Detail = new Controles.BoxViewPage();
+            IsPresented = false;
+        }
+
+        private void GoToLabel(object sender, EventArgs args) {
+            Detail = new Controles.LabelPage();
+            IsPresented = false;
+        }
+
+        private void GoToButton(object sender, EventArgs args) {
+            Detail = new Controles.ButtonPage();
+            IsPresented = false;
+        }
+    }
+}
