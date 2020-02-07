edc99f7ed2026a504f11f19cb56304ebe2e1fa13
Matheus Henrique da Silva <matheustr@hotmail.com>
2020-02-07 18:02:38 -0300

    começo da implementação do Properties

---
+++ App2_Tarefa/App2_Tarefa/Modelos/GerenciadorTarefa.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace App2_Tarefa.Modelos {
+    public class GerenciadorTarefa {
+
+        String key = "keyTarefas";
+
+        //private List<Tarefa> lista { get; set; }
+
+        public void Salvar(Tarefa tarefa) {
+            List<Tarefa> lista;
+            if (App.Current.Properties.ContainsKey(key)) {
+                //lista = (List<Tarefa>)App.Current.Properties.TryGetValue(key, new List<Tarefa>());
+            }
+
+
+
+        }
+
+        public void Finalizar(Tarefa tarefa) {
+
+        }
+
+        public List<Tarefa> Listagem() {
+            return null;
+        }
+
+        public void Remover(Tarefa tarefa) {
+
+        }
+
+        private void SalvarNoProperties(List<Tarefa> lista) {
+
+
+        }
+
+    }
+}
---
+++ App2_Tarefa/App2_Tarefa/Modelos/Tarefa.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace App2_Tarefa.Modelos {
+    public class Tarefa {
+
+        String nome { get; set; }
+        DateTime finalizacao { get; set; }
+        byte prioridade { get; set; }
+    }
+}
