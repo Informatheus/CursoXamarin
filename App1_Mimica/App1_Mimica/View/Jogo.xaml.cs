4729330a49c64bd7f7a982b56d715ccbbbc13a79
Matheus Henrique da Silva <matheustr@hotmail.com>
2020-02-14 13:40:29 -0300

    Inicio do app Mimica - MVVM

---
+++ App1_Mimica/App1_Mimica/View/Jogo.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace App1_Mimica.View {
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Jogo : ContentPage {
+        public Jogo() {
+            InitializeComponent();
+        }
+
+        private void Mostrar_Button_Clicked(object sender, EventArgs e) {
+            (sender as Button).Text = "Iniciar";
+            StackContagem.IsVisible = true;
+        }
+
+        private void Acertou_Button_Clicked(object sender, EventArgs e) {
+
+        }
+
+        private void Errou_Button_Clicked(object sender, EventArgs e) {
+
+        }
+    }
+}
