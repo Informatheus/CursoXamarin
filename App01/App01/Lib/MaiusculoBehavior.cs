d816ad1b51def91c240366083bb903933c9d25e7
Matheus Henrique da Silva <matheustr@hotmail.com>
2020-03-05 21:31:46 -0300

    Toma no cu essa bosta de Trigger

---
+++ App01/App01/Lib/MaiusculoBehavior.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace App01.Lib {
+    public class MaiusculoBehavior : Behavior<Entry> {
+
+        protected override void OnAttachedTo(Entry bindable) {
+            base.OnAttachedTo(bindable);
+            bindable.TextChanged += MaiusculoAction;
+        }
+
+        protected override void OnDetachingFrom(Entry bindable) {
+            base.OnDetachingFrom(bindable);
+            bindable.TextChanged -= MaiusculoAction;
+        }
+
+        private void MaiusculoAction(object sender, TextChangedEventArgs e) {
+            if (!string.IsNullOrEmpty(e.NewTextValue)) {
+                (sender as Entry).Text = e.NewTextValue.ToUpper();
+            }
+        }
+    }
+}
